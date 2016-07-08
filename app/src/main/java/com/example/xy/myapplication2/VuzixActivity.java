da990c75745fa04eeac111b2c1823b584db6ae2b
xiongyuan <yuan.xiong@intel.com>
2016-07-08 16:56:05 +0800

    Vuzix M100 Speech Recognition First Commit

---
+++ app/src/main/java/com/example/xy/myapplication2/VuzixActivity.java
@@ -0,0 +1,16 @@
+package com.example.xy.myapplication2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.vuzix.speech.MainActivity;
+
+public class VuzixActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
